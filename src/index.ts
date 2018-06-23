13b11640f34167a0b780a93f8651e1c600c3c083
Bert Maurau <hello@bertmaurau.be>
2018-06-23 22:02:16 +0200

    added render size and feedback display

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,54 @@
+
+/**
+ * The passable configuration for the Sprite itself
+ */
 interface SpriteOptions {
 
    width: number;
+   renderWidth: number;
    height: number;
+   renderHeight: number;
    image: HTMLImageElement;
    ticksPerFrame: number;
    numberOfFrames: number;
 
 }
 
+/**
+ * The actual Sprite object
+ */
 class Sprite {
 
+   // basics
    private context: CanvasRenderingContext2D;
    private width: number;
+   private renderWidth: number;
    private height: number;
+   private renderHeight: number;
    private image: HTMLImageElement;
 
+   // animation properties
+   private totalTicks: number = 0;
+   private totalFrames: number = 0;
+   private totalLoops: number = 0;
    private frameIndex: number = 0;
    private tickCount: number = 0;
    private ticksPerFrame: number = 5;
    private numberOfFrames: number = 1;
 
+   /**
+    * Constructor
+    * @param {CanvasRenderingContext2D} context 
+    * @param {SpriteOptions} options 
+    */
    constructor(context: CanvasRenderingContext2D, options: SpriteOptions) {
 
       // setters
       this.context = context;
       this.width = options.width;
+      this.renderWidth = options.renderWidth;
       this.height = options.height;
+      this.renderHeight = options.renderHeight;
       this.ticksPerFrame = options.ticksPerFrame;
       this.numberOfFrames = options.numberOfFrames;
 
@@ -40,28 +63,20 @@ class Sprite {
    public render(): void {
 
       // clear the canvas
-      this.context.clearRect(0, 0, this.width, this.height);
+      this.context.clearRect(0, 0, this.renderWidth, this.renderHeight);
 
       // Draw the animation
       this.context.drawImage(
-         // Source image object (Sprite sheet)
-         this.image,
-         // Source x (Frame index times frame width)
-         (this.frameIndex * (this.width / this.numberOfFrames)),
-         // Source y (0)
-         0,
-         // Source width (Frame width)
-         (this.width / this.numberOfFrames),
-         // Source height (Frame height)
-         this.height,
-         // Destination x (0)
-         0,
-         // Destination y (0)
-         0,
-         // Destination width (Frame width)
-         (this.width / this.numberOfFrames),
-         // Destination height (Frame height)
-         this.height);
+
+         this.image,                                              // Source image object (Sprite sheet)
+         (this.frameIndex * (this.width / this.numberOfFrames)),  // Source x (Frame index times frame width)
+         0,                                                       // Source y (0)
+         (this.width / this.numberOfFrames),                      // Source width (Frame width)
+         this.height,                                             // Source height (Frame height)
+         0,                                                       // Destination x (0)
+         0,                                                       // Destination y (0)
+         (this.renderWidth),                                      // Destination width (Frame width)
+         this.renderHeight);                                      // Destination height (Frame height)
    };
 
 
@@ -71,33 +86,57 @@ class Sprite {
    public update(): void {
 
       this.tickCount += 1;
-
-      console.log(`Tick: ${this.tickCount} | PerFrame: ${this.ticksPerFrame}`)
+      this.totalTicks += 1;
 
       if (this.tickCount > this.ticksPerFrame) {
 
          this.tickCount = 0;
+         this.totalFrames += 1;
 
          // if the current frame index is in range
          if (this.frameIndex < (this.numberOfFrames - 1)) {
             // go to the next frame
             this.frameIndex += 1;
-            console.log('Next frame..')
          } else {
+            this.totalLoops +=1;
             this.frameIndex = 0;
-            console.log('First frame..')
-
          }
 
       }
 
    };
 
+   /**
+    * Return the basic information
+    */
+   public getAnimationInfo(): any {
+      return {
+         total_ticks: this.totalTicks,
+         total_frames: this.totalFrames,
+         total_loops: this.totalLoops,
+         current_tick: this.tickCount,
+         current_frame: this.frameIndex,
+         sprite_width: this.width,
+         sprite_height: this.height,
+         sprite_frames: this.numberOfFrames,
+         sprite_ticks_per_frame: this.ticksPerFrame,
+         render_width: this.renderWidth,
+         render_height: this.renderHeight,
+         ticks_per_frame: this.ticksPerFrame,
+      }
+   }
+
 
 }
 
+/**
+ * Main SpriteAnimation class
+ */
 class SpriteAnimation {
 
+   // the div that holds the info
+   private feedbackElement: HTMLDivElement;
+   // the image object
    private image: HTMLImageElement;
    // the canvas used for rendering the sprite
    private canvas: HTMLCanvasElement;
@@ -114,12 +153,17 @@ class SpriteAnimation {
     * @param {number} width width of the canvas
     * @param {number} height height of the convas
     */
-   public load(image: HTMLImageElement, width: number, height: number, ticksPerFrame: number, numberOfFrames: number): SpriteAnimation {
+   public load(image: HTMLImageElement, width: number, height: number, options: any): SpriteAnimation {
+
+
+      // load the element for the display
+      this.feedbackElement = <HTMLDivElement> document.createElement("feedback");
+      document.body.appendChild(this.feedbackElement);
 
       // load the canvas
       this.canvas = <HTMLCanvasElement>document.createElement("canvas");
-      this.canvas.width = width;
-      this.canvas.height = height;
+      this.canvas.width = (options.renderWidth || width);
+      this.canvas.height = (options.renderHeight || height);
       // add the canvas to the body
       document.body.appendChild(this.canvas);
 
@@ -129,24 +173,29 @@ class SpriteAnimation {
       // declare a new sprite
       let spriteOptions: SpriteOptions = {
          width: width,
+         renderWidth: options.renderWidth,
          height: height,
+         renderHeight: options.renderHeight,
          image: image,
-         ticksPerFrame: ticksPerFrame,
-         numberOfFrames: numberOfFrames,
+         ticksPerFrame: options.ticksPerFrame,
+         numberOfFrames: options.numberOfFrames,
       }
       this.sprite = new Sprite(this.canvas.getContext("2d"), spriteOptions);
 
       /**
        * The gameloop to take care of the updates
        */
-      var self = this;
+      this.gameLoop = () => {
 
-      self.gameLoop = () => {
+         window.requestAnimationFrame(this.gameLoop);
 
-         window.requestAnimationFrame(self.gameLoop);
-
+         // calculate new frame to show
          this.sprite.update();
+         // render the actual frame
          this.sprite.render();
+
+         // do other stuff here
+         this.updateFeedback();
       }
 
       // start the game loop as soon as the sprite sheet is loaded
@@ -156,10 +205,15 @@ class SpriteAnimation {
    }
 
    /**
-    * Render the sprite
+    * Update the div with the information
     */
-   public render(): void {
-      this.sprite.render();
+   private updateFeedback(): void {
+
+      let info = this.sprite.getAnimationInfo();
+      this.feedbackElement.innerHTML = `
+         <pre>${JSON.stringify(info, undefined, 2)}</pre> 
+      `;
+      
    }
 
 }
